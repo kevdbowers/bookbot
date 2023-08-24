90788dc02cf9ace4e20755bbafe677e839e2fe7d
kevdbowers <kevdbowers@gmail.com>
2023-08-24 10:35:06 -0400

    added notes, separated and sorted out letters in alphabetical order, added report

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-with open('books/frankenstein.txt') as f:
+source = 'books/frankenstein.txt'  #creating variable for source text
+
+with open(source) as f:   #obtain source text and convert to string
     file_contents = f.read()
 
-words = file_contents.split()
-letter_count = {}
+words = file_contents.split()   #separate string on whitespace into list
+word_count = len(words) #count words
+letter_count = {}   #create dictionary to index letters with count
 
-for word in words:
+for word in words:  #statement separating characters from list then counting, case insensitive
     for letter in word:
         lowercase = letter.lower()
         if lowercase in letter_count:
@@ -12,4 +15,18 @@ for word in words:
         else:
             letter_count[lowercase] = 1
 
-print(letter_count)
+letter_list = []    #creating list solely for letters
+
+for letter in letter_count: #separating letters into intended list
+    if letter.isalpha():
+        letter_list.append(letter)
+
+letter_list.sort()  #sorting letters
+
+print(f"--- Begin report of {source} ---")  #print report on source text
+print(f"{word_count} words found in the document")
+print("")
+
+for letter in letter_list:
+    print(f"The {letter} character was found {letter_count[letter]} times")
+print("--- End report ---")
